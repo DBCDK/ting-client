749213cde401c5701c4f19595b2391d1469951ce
pjohans <pjohans@gmail.com>
2013-03-18 14:34:20 +0100

    auto formatting

--- lib/request/TingClientRequest.php
+++ lib/request/TingClientRequest.php
@@ -1,47 +1,51 @@
 <?php
 
-interface ITingClientRequestCache{
+interface ITingClientRequestCache {
+
   public function cacheKey();
-  public function cacheEnable($value=NULL);
-  public function cacheTimeout($value=NULL);
+
+  public function cacheEnable($value = NULL);
+
+  public function cacheTimeout($value = NULL);
+
   public function cacheBin();
 }
 
 abstract class TingClientRequest {
   /* suffixes to use for cache variables */
-  const cache_lifetime  = '_cache_lifetime';
+
+  const cache_lifetime = '_cache_lifetime';
   const cache_enable = '_cache_enable';
 
   /* attributes to be used by extending classes */
+
   protected $cacheKey;
   private $nameSpace;
   private $wsdlUrl;
-  
   private $parameters = array();
 
   abstract public function processResponse(stdClass $response);
 
   abstract protected function getRequest();
-  
+
   // default implementation of ITingClientRequestCache::cacheBin
   // extending request can implement this method if it wishes it's own bin
   public function cacheBin() {
     return 'cache_bibdk_webservices';
   }
 
-  public function __construct($wsdlUrl, $serviceName=NULL) {
+  public function __construct($wsdlUrl, $serviceName = NULL) {
     $this->wsdlUrl = $wsdlUrl;
   }
-  
-  public function setXsdNameSpace($nameSpace){
+
+  public function setXsdNameSpace($nameSpace) {
     $this->nameSpace = $nameSpace;
-  } 
-  
+  }
+
   public function getXsdNamespace() {
     return $this->nameSpace;
   }
-  
-  
+
   public function setwsdlUrl($wsdlUrl) {
     $this->wsdlUrl = $wsdlUrl;
   }
@@ -49,9 +53,9 @@ abstract class TingClientRequest {
   public function setParameter($name, $value) {
     $this->parameters[$name] = $value;
   }
-  
-  protected function unsetParameter($name){
-    if( isset($this->parameters[$name])) {
+
+  protected function unsetParameter($name) {
+    if (isset($this->parameters[$name])) {
       unset($this->parameters[$name]);
     }
   }
@@ -66,22 +70,20 @@ abstract class TingClientRequest {
     $this->parameters = $array;
   }
 
- /** @TODO refactor. these two methods does not belong here 
- *  move to extending classes. refactor away the 'methodParameterMap'-method
-  * in extending classes .. all they do is map 
-  * numresults to something else  
- **/ 
-  
+  /** @TODO refactor. these two methods does not belong here 
+   *  move to extending classes. refactor away the 'methodParameterMap'-method
+   * in extending classes .. all they do is map 
+   * numresults to something else  
+   * */
   public function getNumResults() {
     return $this->numResults;
-  }  
- 
+  }
+
   public function setNumResults($numResults) {
     $this->numResults = $numResults;
   }
-  
-  /**   **/
 
+  /**   * */
   public function getWsdlUrl() {
     return $this->wsdlUrl;
   }
@@ -98,11 +100,11 @@ abstract class TingClientRequest {
     $response = json_decode($responseString);
 
     if (!$response) {
-      throw new TingClientException('Unable to decode response as JSON: '.$responseString);
+      throw new TingClientException('Unable to decode response as JSON: ' . $responseString);
     }
 
     if (!is_object($response)) {
-      throw new TingClientException('Unexpected JSON response: '.var_export($response, true));
+      throw new TingClientException('Unexpected JSON response: ' . var_export($response, true));
     }
     return $this->processResponse($response);
   }
@@ -129,7 +131,7 @@ abstract class TingClientRequest {
 
   protected static function getAttribute($object, $attributeName) {
     //ensure that attribute names are prefixed with @
-    $attributeName = ($attributeName[0] != '@') ? '@'.$attributeName : $attributeName;
+    $attributeName = ($attributeName[0] != '@') ? '@' . $attributeName : $attributeName;
     return self::getBadgerFishValue($object, $attributeName);
   }
 
@@ -155,4 +157,5 @@ abstract class TingClientRequest {
       return NULL;
     }
   }
+
 }
