623f75b2d064e5a9979167abb106e9525205c968
pjohans <pjohans@gmail.com>
2014-08-29 13:49:22 +0200

    proper exceptions thrown on failure in constructor + comments on methods

--- lib/soapClient/TingSoapClient.php
+++ lib/soapClient/TingSoapClient.php
@@ -2,6 +2,8 @@
 class TingSoapClient{
   private $soapClient;
   public $requestBodyString;
+  // this is for integration with ting-client
+  // @see tingClientRequestAdapter, @see contrib/nanosoap.inc
   private $curl_info;
   // for test purpose
   public static $user_agent;
@@ -9,6 +11,7 @@ class TingSoapClient{
   public function __construct($request, $location = NULL){
     // get uri of wsdl
     $wsdl = $request->getWsdlUrl();
+    debug($wsdl);
     // soapClient is set with trace and exception options to enable proper exceptionhandling and logging
     $options = array(
       'trace' => 1,
@@ -24,39 +27,33 @@ class TingSoapClient{
     if(!empty($location)) {
       $options += array('location'=>$location);
     }
-
-    try{
-      // developer note: xdebug causes soapclient to fail FATAL before
-      // soapclient throws an exception - disable it. it shouldn't be in
-      // production anyways
-      if(function_exists('xdebug_disable')){
-        xdebug_disable();
-      }
-      $this->soapClient = @new SoapClient($wsdl,$options);
+    // xdebug causes a fatal error before soapclient handles error in constructor
+    // disable it. it shouldn't be in production anyways
+    if(function_exists('xdebug_disable')){
+      xdebug_disable();
     }
-    catch(Exception $e){
-      // set status code to 500 (server error)
-      $this->set_curl_info('500');
+    // constructor causes an php i/o warning on failure. suppress it (@)
+    $this->soapClient = @new SoapClient($wsdl,$options);
+
+    if(!is_object($this->soapClient) || is_soap_fault($this->soapClient)){
+      throw new SoapFault('500','SoapClientFault:Failed to construct. WSDL location is:'.$wsdl);
     }
   }
 
+  /** wrapper for execution of scoapclient.
+   * this is for integration with ting-client
+   * @param string $action; the method to execute
+   * @param mixed $params; paramters for method
+   * @return mixed bool | stdClass
+   */
   public function call($action, $params){
-    $data = $this->send_request($action, $params);
-    return $data;
-  }
-
-  public function getCurlInfo(){
-    return $this->curl_info;
-  }
-
-  private function send_request($action,$params){
     try{
       $data = $this->soapClient->$action($params);
     }
     catch(Exception $e){
       // set status code to 400 (bad request)
       $this->set_curl_info('400');
-      return;
+      return FALSE;
     }
 
     // all went well
@@ -66,6 +63,24 @@ class TingSoapClient{
     return $data;
   }
 
+  /** Return status for request
+   * this is for integration with ting-client
+   * @see tingClientRequestAdapter, @see contrib/nanosoap.inc
+   *
+   * return private member curl_info
+   */
+  public function getCurlInfo(){
+    return $this->curl_info;
+  }
+
+  /** Set private member curl_info with given errorcode. If errorcode is not set
+   *  it is assumed that the request has completed, and the curl_info is set
+   *  from response headers.
+   *
+   * for integration with ting-client
+   *
+   * @param int null $errorcode;
+   */
   private function set_curl_info($errorcode = NULL){
     if(!empty($errorcode)){
       $this->curl_info = array('http_code'=>$errorcode);
