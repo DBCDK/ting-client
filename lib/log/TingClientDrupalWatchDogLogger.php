573263a02694e7742882a51abf130e781cc85642
pjo <pjo@dbc.dk>
2020-08-13 09:10:41 +0200

    formatting

--- lib/log/TingClientDrupalWatchDogLogger.php
+++ lib/log/TingClientDrupalWatchDogLogger.php
@@ -5,15 +5,17 @@
  *
  * @see http://api.drupal.org/api/function/watchdog/
  */
-class TingClientDrupalWatchDogLogger extends TingClientLogger {
-  public function doLog($message_type, $variables, $severity) {
+class TingClientDrupalWatchDogLogger extends TingClientLogger
+{
+  public function doLog($message_type, $variables, $severity)
+  {
     $log_type = 'ting client';
     // we want a seperate log type for slow requests
-    if($this->log_time > 2.0){
+    if ($this->log_time > 2.0) {
       $log_type = 'SLA alert';
     }
     $variables['time'] = $this->log_time;
-    switch($message_type){
+    switch ($message_type) {
       case 'soap_request_complete':
         $message = 'Completed @adapter request @action @wsdlUrl ( @time s). Request body: @requestBody';
         if (!empty($variables['requestVariables'])) {
@@ -27,9 +29,9 @@ class TingClientDrupalWatchDogLogger extends TingClientLogger {
         $message = '@type request @action @wsdlUrl ( @time s). Request body: @requestBody';
     }
 
-    watchdog($log_type,$message, $variables,
-             constant('WATCHDOG_' . $severity),
-             'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    watchdog($log_type, $message, $variables,
+      constant('WATCHDOG_' . $severity),
+      'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
   }
 }
 
